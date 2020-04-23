841759a7e50131836bb322b1a5d88768d0eb5596
Saranawab <60245359+Saranawab@users.noreply.github.com>
2020-04-23 13:48:30 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>SARA NAWAB's | Beauty Parlour</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="SARA NAWAB's | Beauty Parlour">
+	<meta name="keywords" content="SARA NAWAB's | Beauty Parlour">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark site-navbar">
+			<a class="navbar-brand site-logo" href="index.html#">
+				<h2><span>SARA </span>Nawab's</h2>
+				<p><center>BEAUTY PARLOUR</center></p>
+			</a>
+			<button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+				aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="collapsibleNavId">
+				<!-- Main menu -->
+				<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Contact Us</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">More</a>
+					</li>                                                                
+				</ul>
+				<div class="social-links my-2 my-lg-0">
+					<a href="#"><i class="fa fa-pinterest"></i></a>
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- Header section end-->
+
+	<!-- Hero section -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hs-item set-bg" data-setbg="img/slider/1.jpg" data-hash="slide-1">
+				<div class="container">
+					<h2>Welcome to our Parlour</h2>
+					<a href="#" class="site-btn">Read More <i class="fa fa-angle-double-right"></i></a>
+				</div>
+				<div class="next-hs set-bg" data-setbg="img/slider/2.jpg">
+					<a href="#slide-2" class="nest-hs-btn">Next</a>
+				</div>
+			</div>
+			<div class="hs-item set-bg" data-setbg="img/slider/2.jpg" data-hash="slide-2">
+				<div class="container">
+					<h2>Welcome to our Parlour</h2>
+					<a href="#" class="site-btn">Read More <i class="fa fa-angle-double-right"></i></a>
+				</div>
+				<div class="next-hs set-bg" data-setbg="img/slider/1.jpg">
+					<a href="#slide-1" class="nest-hs-btn">Next</a>
+				</div>
+			</div>
+		</div>
+		
+	</section>
+	<!-- Hero section end-->
+
+	<!-- Intro section -->
+	<section class="intro-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5">
+					<img src="img/intro-img.jpg" alt="">
+				</div>
+				<div class="col-lg-7 intro-text">
+					<span>Words by Sara Nawab,</span>
+					<h2>"At the Beauty Parlour, we believe in beauty with a conscience."</h2>
+					<p>Our design and philosophy work hand in hand. We are committed to providing healthy hair care with a low impact on the environment. Come see what you and your hair can do for the Earth.</p>
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Intro section end-->
+
+	<!-- Portfolio section -->
+	<section class="portfolio-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-7">
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/1.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-5">
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/2.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+				</div>
+				<div class="col-lg-5 col-md-12">
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/3.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+				</div>
+				<div class="col-lg-6 col-md-12">
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/4.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/5.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/7.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/6.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/8.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/9.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/10.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/11.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+				</div>
+				<div class="col-lg-6 col-md-12">
+					<a href="#" class="portfolio-item">
+						<img src="img/portfolio/12.jpg" alt="">
+						<h4>See More</h4>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Portfolio section end-->
+
+	<!-- Blog section -->
+	<section class="blog-section spad">
+		<div class="container">
+			<div class="blog-slider owl-carousel">
+				<div class="blog-item">
+					<div class="blog-thumb set-bg" data-setbg="img/portfolio/12.jpg"">
+						<div class="blog-date">
+							<h2>20</h2>
+							<p>March</p>									
+						</div>
+					</div>
+					<div class="blog-head">
+						<div class="blog-tags">fashion, event, lifestyle</div>
+						<h2><a href="#">Most stunning bridal jewellery sets</a></h2>
+					</div>
+					<p>A woman can never deny any piece of jewelry that has given to her or she herself bought it. We can say that jewelry is the best friend of every woman. It will sound a bit odd but some women feel satisfaction if they have a huge collection of the latest and trendy pieces of jewelry.</p>
+				</div>
+				<div class="blog-item">
+					<div class="blog-thumb set-bg" data-setbg="img/portfolio/5.jpg"">
+						<div class="blog-date">
+							<h2>20</h2>
+							<p>March</p>									
+						</div>
+					</div>
+					<div class="blog-head">
+						<div class="blog-tags">fashion, event, lifestyle</div>
+						<h2><a href="#">Top Canadian Designers’ Boutiques in Regina </a></h2>
+					</div>
+					<p>It is very tough to find a boutique that makes perfect bridal outfits. To keep you out of this fuss we list down 5 top notch boutiques in Regina for wedding dress shopping.</p>
+				</div>
+				<div class="blog-item">
+					<div class="blog-thumb set-bg" data-setbg="img/portfolio/6.jpg">
+						<div class="blog-date">
+							<h2>20</h2>
+							<p>March</p>									
+						</div>
+					</div>
+					<div class="blog-head">
+						<div class="blog-tags">fashion, event, lifestyle</div>
+						<h2><a href="#">Cushion Cut Diamond Engagement Rings</a></h2>
+					</div>
+					<p>Cushion cut diamond rings are being admired by every bride-to-be because of the wide sparkling effect that this cut gives to the ring. It is one of the oldest diamond cuts in the history which means this cut will not only look classy but also illustrate the historical sparkles.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Blog section end-->
+
+	<!-- Back to top -->
+	<div class="container">
+		<div class="backtotop">
+			<div class="up-btn" id="backTotop">UP</div>
+		</div>
+	</div>
+
+	<!-- Footer section -->
+	<footer class="footer-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget fw-about">
+						<img src="img/footer-logo.png" alt="">
+						
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget resent-post">
+						<h2 class="fw-title">Recent Posts</h2>
+						<div class="rp-item">
+							<h4>Regina Wedding Week</h4>
+							<span>20 March 2020</span>
+						</div>
+						<div class="rp-item">
+							<h4>About Our Beauty Parlour</h4>
+							<span>20 March 2020</span>
+						</div>
+						<div class="rp-item">
+							<h4>Simple Blog Post</h4>
+							<span>20 March 2020</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Usefull Links</h2>
+						<ul>
+							<li><a href="#">Testimonials</a></li>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Jobs</a></li>
+							<li><a href="#">Reviews</a></li>
+							<li><a href="#">Marketing</a></li>
+							<li><a href="#">Subscribe</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget contact-widget">
+						<h2 class="fw-title">Contact</h2>
+						<ul>
+							<li><span>Address:</span>Regina,SK</li>
+							<li><span>Phone:</span>(306) 999 0494</li>
+							<li><span>Mail:</span>abidsara32@gmail.com</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+					<center>
+
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | SARA NAWAB'S BEAUTY PARLOUR WEBSITE <i class="fa fa-heart-o" aria-hidden="true"></i>!!<a href="#" target="_blank">!!</a>
+
+					</center>
+		</div>
+	</footer>
+	<!-- Footer section end -->
+	
+	
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
